58baaafc359e757f0435a5c8ff04ea690019b078
duskGeek <499266134@qq.com>
2020-07-16 01:28:21 +0800

    sourcefunction实现

---
+++ geek-flink/src/main/scala/com/geek/quickStart/SourceFunctions/ListOutFunction.scala
@@ -0,0 +1,10 @@
+package com.geek.quickStart.SourceFunctions
+
+import org.apache.flink.streaming.api.functions.source.{ParallelSourceFunction, SourceFunction}
+
+class ListOutFunction extends ParallelSourceFunction<>{
+
+}
+
+
+case class Access(domain:String,)
--- geek-flink/src/main/scala/com/geek/quickStart/StreamJob.scala
+++ geek-flink/src/main/scala/com/geek/quickStart/StreamJob.scala
@@ -1,11 +1,35 @@
 package com.geek.quickStart
 
+import org.apache.flink.api.java.tuple.Tuple
+import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.windowing.time.Time
+
 object StreamJob {
 
   def main(args: Array[String]): Unit = {
+    val param=ParameterTool.fromArgs(args)
 
+    val port=param.getRequired("port")
+    val host=param.getRequired("host")
 
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
 
+    val stream=env.socketTextStream(host,port.toInt)
+    val value: KeyedStream[Wc, Tuple] = stream.flatMap(x => {
+      x.split(',')
+    }).map(x => {
+      Wc(x,1)
+    }).
+      keyBy("text")
+
+    value.
+      timeWindow(Time.seconds(5)).
+      sum("num").print()
+
+    env.execute(this.getClass.getSimpleName)
   }
 
+  case class Wc(text:String,num:Int)
 }
