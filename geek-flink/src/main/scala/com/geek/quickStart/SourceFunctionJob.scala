3106cb4810707703571e1a2fae3837bafa4bc36c
yeqian <499266134@qq.com>
2020-07-16 22:13:32 +0800

    flinksourcefunction

--- geek-flink/src/main/scala/com/geek/quickStart/SourceFunctionJob.scala
+++ geek-flink/src/main/scala/com/geek/quickStart/SourceFunctionJob.scala
@@ -2,11 +2,20 @@ package com.geek.quickStart
 
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 
+import java.util.Random
+
 object SourceFunctionJob {
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
 
+    val random= new Random(3)
 
+
+
+    System.out.println(random.nextInt(3))
+    System.out.println(random.nextInt(3))
+    System.out.println(random.nextInt(3))
+    System.out.println(random.nextInt(3))
 
 
 
--- geek-flink/src/main/scala/com/geek/quickStart/SourceFunctions/ListOutFunction.scala
+++ geek-flink/src/main/scala/com/geek/quickStart/SourceFunctions/ListOutFunction.scala
@@ -1,10 +1,33 @@
 package com.geek.quickStart.SourceFunctions
 
-import org.apache.flink.streaming.api.functions.source.{ParallelSourceFunction, SourceFunction}
+import org.apache.flink.streaming.api.functions.source.{RichParallelSourceFunction, SourceFunction}
 
-class ListOutFunction extends ParallelSourceFunction<>{
+import scala.util.Random
 
+class ListOutFunction extends RichParallelSourceFunction[Access] {
+
+  val running=true
+
+  val domains=Array("www.baidu.com","www.taobao.com","www.alibaba.com")
+
+  val random= new Random()
+
+  override def run(sourceContext: SourceFunction.SourceContext[Access]): Unit = {
+    while (running){
+      1.to(10).foreach(x=>{
+        val time=System.currentTimeMillis()
+        val access=Access(domains(random.nextInt(3)),time,(x*random.nextInt(100)))
+        sourceContext.collect(access)
+      })
+      Thread.sleep(5000)
+    }
+  }
+
+  override def cancel(): Unit = {
+  }
 }
 
 
-case class Access(domain:String,)
+case class Access(domain:String,time:Long,traffic:Long)
+
+
