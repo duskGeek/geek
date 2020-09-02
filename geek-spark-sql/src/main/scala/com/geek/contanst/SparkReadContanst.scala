157c061eaecf688dabb4fdbca83b6de5182514a2
yeqian <499266134@qq.com>
2020-09-02 20:39:53 +0800

    去除

--- geek-spark-sql/src/main/scala/com/geek/contanst/SparkReadContanst.scala
+++ geek-spark-sql/src/main/scala/com/geek/contanst/SparkReadContanst.scala
@@ -4,7 +4,7 @@ object SparkReadContanst {
    val MYSQL_DRIVER_CLASS_NAME="com.mysql.jdbc.Driver"
 
   //易车TIDB集群 读连接
-   val YC_TIDB_READ_URL="jdbc:mysql://172.23.0.39:4000/mocard_flw?useUnicode=true&characterEncoding=utf-8&useSSL=false"
-   val YC_TIDB_READ_USER="mocard_rw"
+   val YC_TIDB_READ_URL=""
+   val YC_TIDB_READ_USER=""
    val YC_TIDB_READ_PASSWORD="wsxQASW!"
 }
