e7f959fe09e102b365d65b7d61b2b793d11f99f7
yeqian <499266134@qq.com>
2022-01-10 17:30:20 +0800

    删除

--- geek-utils/src/test/java/log4j/Log4jTest.java
+++ geek-utils/src/test/java/log4j/Log4jTest.java
@@ -1,7 +1,16 @@
 package log4j;
 
+
+import com.amazonaws.util.Base64;
+
 public class Log4jTest {
     public static void main(String[] args) {
 
+        String str="e4264881ca550293eb2539709c705972";
+        byte[] a=Base64.decode(str);
+
+        for (byte b: a) {
+            System.out.println(b);
+        }
     }
 }
